83e8d6dc90536ae1f8553d1ed4ad9d7e8272ed55
cmknezev <cmknezev@ncsu.edu>
2022-07-21 02:14:17 -0400

    numeric summaries

--- PGA-Tour-Project/server.R
+++ PGA-Tour-Project/server.R
@@ -5,7 +5,30 @@ library(tidyverse)
 shinyServer(function(input, output){ 
   
   ### read in & clean data ###
+  
   # read in 
   pgaDat <- read_csv("../pgaTourData.csv")
+  # removing extraneous variables 
+  pgaDat <- pgaDat[, -c(11, 12, 18)]
+  # renaming columns to more friendly names 
+  colnames(pgaDat) <- c("name", "rounds", "fairwayPct", "year", "avgDistance", 
+                        "gir", "avgPutts", "avgScrambling", "avgScore", 
+                        "points", "avgSgPutts", "avgSgTotal", "sgOTT", 
+                        "sgAPR", "sgARG")
+  # changing Percentage-type variables to decimal format 
+  pgaDat["fairwayPct"] <- pgaDat["fairwayPct"]/100
+  pgaDat["gir"] <- pgaDat["gir"]/100 
+  pgaDat["avgScrambling"] <- pgaDat["avgScrambling"]/100
+  # remove NA's from points variable 
+  pgaDat$points <- ifelse(is.na(pgaDat$points), 0, pgaDat$points)
+  
+  ### data exploration ### 
+  
+  # numeric summary
+  output$numSumm <- renderPrint({ 
+    subDat <- pgaDat %>% filter(name == input$numSummPlayer)
+    var <- input$numSummVar 
+    summary(subDat[var])
+  })
   
 })
--- PGA-Tour-Project/ui.R
+++ PGA-Tour-Project/ui.R
@@ -13,9 +13,30 @@ shinyUI(navbarPage("PGA Tour App",
       ),
       mainPanel("picture")
     ), 
-  )
+  ),
   
   # Data Exploration UI
+  tabPanel("Data Exploration Page", 
+    titlePanel("Exploratory Data Analysis"), 
+    sidebarLayout(
+      textInput("numSummPlayer", "Player for Numeric Summaries", 
+                value = "Rory McIlroy"), 
+      selectInput("numSummVar", "Variable for Numeric Summaries", 
+                  c("Points" = "points", 
+                    "Fairway %" = "fairwayPct", 
+                    "Driver Distance" = "avgDistance", 
+                    "Greens in Regulation %" = "gir", 
+                    "Putts" = "avgPutts", 
+                    "Scrambling" = "avgScrambling", 
+                    "Score" = "avgScore", 
+                    "Shots Gained: Off the Tee" = "sgOTT", 
+                    "Shots Gained: Approach" = "sgAPR", 
+                    "Shots Gained: Around the Green" = "sgARG"))
+    ), 
+    mainPanel(
+      verbatimTextOutput("numSumm")
+    )
+  )
 ))
 
 #shiny::runGitHub("ST-558-Project-3", "cmknezev", subdir = "PGA-Tour-Project")
